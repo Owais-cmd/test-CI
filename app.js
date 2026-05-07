8db07bd1364029288edbfd8c3b16cc3887c826de
Owais-cmd <owaistab18@gmail.com>
2026-05-07 21:45:21 +0530

    Correct typo in exit function
    Fix typo in process.exit function call.

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const apiKey = process.env.WEATHER_API_KEY;
 
 if (!apiKey) {
   console.error("CRITICAL ERROR: No API Key found!");
-  process.exit(1);
+  prcess.exit(1);
 }
 
 console.log("App is running securely.");
