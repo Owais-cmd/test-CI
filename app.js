662d3997920c2ff298b74b9a77e11963277b5ecd
Owais Saud Tanveer <1ms23cs126@msrit.edu>
2026-05-07 22:56:50 +0530

    Add add function to app.js

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ const apiKey = process.env.WEATHER_API_KEY;
 if (!apiKey) {
   console.error("CRITICAL ERROR: No API Key found!");
   process.exit(1);
+
+  
+}
+function add(x,y){
+  return x+y;
 }
 
 console.log("App is running securely.");
